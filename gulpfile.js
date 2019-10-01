8192df42adbe48a8e3c97d3036af20388f7f51fd
ppisljar <peter.pisljar@gmail.com>
2019-10-01 07:29:00 -0700

    updating packages

--- gulpfile.js
+++ gulpfile.js
@@ -12,24 +12,12 @@ var gzip = require('gulp-gzip');
 const urlPrefixer = require('gulp-url-prefixer');
 var gutil = require('gulp-util');
 
-// Set the browser that you want to support
-const AUTOPREFIXER_BROWSERS = [
-    'ie >= 10',
-    'ie_mob >= 10',
-    'ff >= 30',
-    'chrome >= 34',
-    'safari >= 7',
-    'opera >= 23',
-    'ios >= 7',
-    'android >= 4.4',
-    'bb >= 10'
-  ];
 
   // Gulp task to minify CSS files
 gulp.task('styles', function () {
     return gulp.src('./src/styles.css')
       // Auto-prefix css styles for cross browser compatibility
-      .pipe(autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+      .pipe(autoprefixer())
       // Minify the file
       .pipe(csso())
       // Output
